0b1ac4021d4ebf479792421073e75a1f603166a5
ruschywez <ruschywez@gmail.com>
2026-03-23 13:37:41 +0700

    расширение функционала

--- src/creation/diagonal.rs
+++ src/creation/diagonal.rs
@@ -3,7 +3,7 @@ use num_traits::Num;
 use crate::core::strides::compute_strides;
 
 impl <T: Num + Clone + Copy> Tensor<T> {
-    pub fn eye_2D(size: usize) -> Self {
+    pub fn eye_2_d(size: usize) -> Self {
         let mut values: Vec<T>  = vec![T::zero(); size * size];
         let shapes: Vec<usize> = vec![size, size];
         let strides: Vec<usize> = compute_strides(&shapes);
